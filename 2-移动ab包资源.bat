0f20dfb718fde41482b401c123bb798cbd02a91a
“Eraseries” <1324058099@qq.com>
2023-02-13 23:48:18 +0800

    更新

--- 2-移动ab包资源.bat
+++ 2-移动ab包资源.bat
@@ -2,8 +2,8 @@
 rem 将一个文件夹里的所有文件移动到另一个文件夹里
 set #=Any questions&set _=WX&set $=Q&set/az=0x53b7e0b4
 title %#% +%$%%$%/%_% %z%
-set "oldfolder=F:\MyDemoGame\Android\asset\Package\0_1_0_0\Android"
-set "newfolder=F:\MyDemoGame\Assets\StreamingAssets"
+set "oldfolder=F:\MyGameDemo\Android\asset\Package\0_1_0_0\Android"
+set "newfolder=F:\MyGameDemo\Assets\StreamingAssets"
 if not exist "%oldfolder%" (echo;"%oldfolder%" path error or not exist&pause&exit)
 if "%oldfolder:~-1%" equ "\" set "oldfolder=%oldfolder:~,-1%"
 if "%newfolder:~-1%" equ "\" set "newfolder=%newfolder:~,-1%"
